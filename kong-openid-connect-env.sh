2856d15dd98b4512a570943eb5bd55e3a83383ca
Ganga Sah <ganga.sah@gmail.com>
2022-08-28 11:39:23 -0700

    Added env for Keycloak v19.0.1

--- kong-openid-connect-env.sh
+++ kong-openid-connect-env.sh
@@ -1,4 +1,4 @@
-HOST_SUFFIX=926a5c04
+HOST_SUFFIX=85480ddc
 KONG_ADMIN_API_URI=https://8001-0-${HOST_SUFFIX}.labs.konghq.com
 KONG_SERVICE_URI=https://8000-0-${HOST_SUFFIX}.labs.konghq.com
 
@@ -8,16 +8,20 @@ USERNAME=employee
 PASSWORD=test
 
 # Kong Keyclock
-ROUTE=keycloak-kong
-CONFIG_ISSUER=https://8080-1-${HOST_SUFFIX}.labs.konghq.com/auth/realms/kong/.well-known/openid-configuration
-CLIENT_ID=kong
-CLIENT_SECRET=681d81ee-9ff0-438a-8eca-e9a4f892a96b
+#ROUTE=keycloak-kong
+#CONFIG_ISSUER=https://8080-1-${HOST_SUFFIX}.labs.konghq.com/auth/realms/kong/.well-known/openid-configuration
+#CLIENT_ID=kong
+#CLIENT_SECRET=681d81ee-9ff0-438a-8eca-e9a4f892a96b
 
 # Local Keyclock
 ROUTE=keycloak-local
-CONFIG_ISSUER=https://73f8-2601-646-8100-5160-2509-4d3f-ba57-d4c9.ngrok.io/auth/realms/master/.well-known/openid-configuration
 CLIENT_ID=kong
-CLIENT_SECRET=HjMJ1bRfq26ya1gSlf3C8IqU0GkN0mHO
+# Keycloak 16.1.1
+#CONFIG_ISSUER=https://73f8-2601-646-8100-5160-2509-4d3f-ba57-d4c9.ngrok.io/auth/realms/master/.well-known/openid-configuration
+#CLIENT_SECRET=HjMJ1bRfq26ya1gSlf3C8IqU0GkN0mHO
+# Keycloak 19.0.1
+CONFIG_ISSUER=https://73f8-2601-646-8100-5160-2509-4d3f-ba57-d4c9.ngrok.io/realms/master/.well-known/openid-configuration
+CLIENT_SECRET=ZmmdpFd72yqM3e0hfFJDbM2kBpRWcQOk
 
 # Auth0
 #ROUTE=oidc-auth0
