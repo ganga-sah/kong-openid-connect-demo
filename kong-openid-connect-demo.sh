bca4383681c461a4381d6097d06f4fd741339592
Ganga Sah <ganga.sah@gmail.com>
2022-09-04 21:43:08 -0700

    kong route to spring oauth-resource-server

--- kong-openid-connect-demo.sh
+++ kong-openid-connect-demo.sh
@@ -2,13 +2,13 @@
 set -x o
 
 echo Add a Service
-http put ${KONG_ADMIN_API_URI}/services/${ROUTE}-service url=http://httpbin.org/anything | jq
+http put ${KONG_ADMIN_API_URI}/services/${ROUTE}-service url=$REST_URL | jq
 
 echo Add a Route to expose the Service
 http -f put ${KONG_ADMIN_API_URI}/services/${ROUTE}-service/routes/${ROUTE}-route paths=/${ROUTE} | jq
 
 echo Verifying the service
-http get ${KONG_SERVICE_URI}/${ROUTE} | jq
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} | jq
 
 echo Add OpenID Connect Plugin
 http -f post ${KONG_ADMIN_API_URI}/routes/${ROUTE}-route/plugins \
@@ -18,22 +18,25 @@ config.client_id=$CLIENT_ID \
 config.client_secret=$CLIENT_SECRET \
 config.redirect_uri=$REDIRECT_URI/${ROUTE} \
 config.response_mode=form_post \
-config.ssl_verify=false | jq
+config.ssl_verify=false \
+config.run_on_preflight=false | jq
 
 echo Provide a client_id and secret to retrieve a token
-http get ${KONG_SERVICE_URI}/${ROUTE} -a ${CLIENT_ID}:${CLIENT_SECRET} | jq
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} -a ${CLIENT_ID}:${CLIENT_SECRET} | jq
 
-echo Verify Kong is protecting the Service - http: 302
-http get ${KONG_SERVICE_URI}/${ROUTE} | jq
-
-echo Verify Kong is protecting the Service by providing invalid user credentials - http: 401
-http get ${KONG_SERVICE_URI}/${ROUTE} -a user:password | jq
-
-echo Optional: View Kong Discovery Information from IDP
-http get ${KONG_ADMIN_API_URI}/openid-connect/issuers | jq
+#echo Verify Kong is protecting the Service - http: 302
+#http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} | jq
+#
+#echo Verify Kong is protecting the Service by providing invalid user credentials - http: 401
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} -a user:password | jq
+#
+#echo Optional: View Kong Discovery Information from IDP
+#http get ${KONG_ADMIN_API_URI}/openid-connect/issuers | jq
 
 echo Provide a username/password to Kong and retrieve a token
-http get ${KONG_SERVICE_URI}/${ROUTE} -a ${USERNAME}:${PASSWORD} | jq
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} -a ${USERNAME}:${PASSWORD} | jq
+
+exit
 
 echo Obtain the URL to access the OIDC service via Kong and click on the link
 echo To obtain the URL, run the command and open the URL in browser
@@ -44,7 +47,7 @@ echo  $KONG_SERVICE_URI/${ROUTE}/verify-session
 
 echo Authenticate with bearer token
 export BEARER_TOKEN=$(http ${KONG_SERVICE_URI}/${ROUTE} -a ${USERNAME}:${PASSWORD} | jq -r '.headers .Authorization')
-http get ${KONG_SERVICE_URI}/${ROUTE} Authorization:"$BEARER_TOKEN" | jq
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} Authorization:"$BEARER_TOKEN" | jq
 
 echo Add upstream headers
 PLUGIN_ID=$(http ${KONG_ADMIN_API_URI}/routes/${ROUTE}-route/plugins/ | jq -r '.data[] | select(.name == "openid-connect") | .id')
@@ -63,22 +66,22 @@ PLUGIN_ID=$(http ${KONG_ADMIN_API_URI}/routes/${ROUTE}-route/plugins/ | jq -r '.
 http -f patch ${KONG_ADMIN_API_URI}/plugins/$PLUGIN_ID config.consumer_claim=preferred_username | jq
 
 echo Verify authorization works for a user mapped to a Kong consumer
-http get ${KONG_SERVICE_URI}/${ROUTE} -a ${USERNAME}:${PASSWORD} | jq
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} -a ${USERNAME}:${PASSWORD} | jq
 
 echo Verify authorization is forbidden for a user not mapped to a consumer - http: 403
-http get ${KONG_SERVICE_URI}/${ROUTE} -a partner:test | jq
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} -a partner:test | jq
 
 echo Add Rate Limiting
 http -f ${KONG_ADMIN_API_URI}/consumers/employee/plugins name=rate-limiting config.minute=5 config.policy=local
 echo Access the route with user employee:
-http get ${KONG_SERVICE_URI}/${ROUTE} -a ${USERNAME}:${PASSWORD} | jq
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} -a ${USERNAME}:${PASSWORD} | jq
 
 echo Exceed Rate Limit
-http get ${KONG_SERVICE_URI}/${ROUTE} username=${USERNAME} password=${PASSWORD}
-http get ${KONG_SERVICE_URI}/${ROUTE} username=${USERNAME} password=${PASSWORD}
-http get ${KONG_SERVICE_URI}/${ROUTE} username=${USERNAME} password=${PASSWORD}
-http get ${KONG_SERVICE_URI}/${ROUTE} username=${USERNAME} password=${PASSWORD}
-http get ${KONG_SERVICE_URI}/${ROUTE} username=${USERNAME} password=${PASSWORD}
-http get ${KONG_SERVICE_URI}/${ROUTE} username=${USERNAME} password=${PASSWORD}
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} username=${USERNAME} password=${PASSWORD}
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} username=${USERNAME} password=${PASSWORD}
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} username=${USERNAME} password=${PASSWORD}
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} username=${USERNAME} password=${PASSWORD}
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} username=${USERNAME} password=${PASSWORD}
+http get ${KONG_SERVICE_URI}/${ROUTE}${REST_API} username=${USERNAME} password=${PASSWORD}
 
 
--- kong-openid-connect-env.sh
+++ kong-openid-connect-env.sh
@@ -1,27 +1,34 @@
-HOST_SUFFIX=85480ddc
+HOST_SUFFIX=5acc68e9
 KONG_ADMIN_API_URI=https://8001-0-${HOST_SUFFIX}.labs.konghq.com
 KONG_SERVICE_URI=https://8000-0-${HOST_SUFFIX}.labs.konghq.com
-
+REST_URL=http://73.15.195.77:8081
+#REST_URL=http://httpbin.org/anything
+#REST_URL=https://qa.safeguardcyber.app/safeguard
+REST_API=/resource-server/api/foos/
 # Keycloak
 REDIRECT_URI=https://8000-0-${HOST_SUFFIX}.labs.konghq.com
-USERNAME=employee
-PASSWORD=test
 
 # Kong Keyclock
 #ROUTE=keycloak-kong
 #CONFIG_ISSUER=https://8080-1-${HOST_SUFFIX}.labs.konghq.com/auth/realms/kong/.well-known/openid-configuration
 #CLIENT_ID=kong
 #CLIENT_SECRET=681d81ee-9ff0-438a-8eca-e9a4f892a96b
+#USERNAME=employee
 
 # Local Keyclock
-ROUTE=keycloak-local
-CLIENT_ID=kong
+#ROUTE=keycloak-httpbin
+ROUTE=keycloak-resource-server
+USERNAME=mike@other.com
+PASSWORD=pass
 # Keycloak 16.1.1
 #CONFIG_ISSUER=https://73f8-2601-646-8100-5160-2509-4d3f-ba57-d4c9.ngrok.io/auth/realms/master/.well-known/openid-configuration
 #CLIENT_SECRET=HjMJ1bRfq26ya1gSlf3C8IqU0GkN0mHO
 # Keycloak 19.0.1
-CONFIG_ISSUER=https://73f8-2601-646-8100-5160-2509-4d3f-ba57-d4c9.ngrok.io/realms/master/.well-known/openid-configuration
-CLIENT_SECRET=ZmmdpFd72yqM3e0hfFJDbM2kBpRWcQOk
+CONFIG_ISSUER=https://bd2f-2601-646-8100-5160-7d52-d250-f546-8071.ngrok.io/auth/realms/baeldung/.well-known/openid-configuration
+CLIENT_ID=newClient
+CLIENT_SECRET=newClientSecret
+#CLIENT_SECRET=ZmmdpFd72yqM3e0hfFJDbM2kBpRWcQOk
+#CLIENT_SECRET=O56JP1GoMpHRDJAPTZ5OOLUBtOIhHp4j
 
 # Auth0
 #ROUTE=oidc-auth0
